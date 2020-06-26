6d09bdfa1c7300a9de7cfd652978f4216123d199
fixe96 <lueders.felix@gmail.com>
2020-06-26 16:10:46 +0200

    [TASK] Operations-States hinzugefügt

---
+++ Assets/Scripts/Behaviour/Rotation.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    private Vector3 axis;
+    private float alpha;
+    
+    /// <summary>
+    /// Physics Update.
+    /// </summary>
+    private void FixedUpdate()
+    {
+    }
+    
+    /// <summary>
+    /// Passing rotation as input where IP-Paket-Piece should rotate to.
+    /// </summary>
+    /// <param name="axis">Axis - rotation axis</param>
+    /// <param name="alpha">Alpha - rotation angle</param>
+    public void Rotate(Vector3 axis, float alpha)
+    {
+        this.axis = axis;
+        this.alpha = alpha;
+        
+        //transform.RotateAround(pseudoWorldCoordinateSystem.transform.position, this.axis, this.alpha);
+    }
+
+}
---
+++ Assets/Scripts/Behaviour/Scaling.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scaling : MonoBehaviour
+{
+    private Vector3 scale;
+    
+    /// <summary>
+    /// Physics Update.
+    /// </summary>
+    private void FixedUpdate()
+    {
+    }
+
+    public void Scale(Transform koordsystem, float scaleX, float scaleY, float scaleZ) 
+    {
+        this.transform.SetParent(koordsystem);
+        Vector3 scale = koordsystem.localScale;
+        scale.x = scaleX;
+        scale.y = scaleY;
+        scale.z = scaleZ;
+
+        this.scale = scale;
+
+        koordsystem.localScale = this.scale; 
+    }
+    
+    public void Scale(Transform koordsystem, Vector3 scale) 
+    {
+        this.scale = scale;
+        this.transform.SetParent(koordsystem);
+
+        koordsystem.localScale = this.scale; 
+    }
+}
